3f59a2ed1a0ce499f9af5ab36960b62f84c552a8
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-03-05 11:38:00 -0600

    Save mock data for testing purposes.

---
+++ src/assets/MockData/RolesData.tsx
@@ -0,0 +1,68 @@
+
+const ROLES = [
+    {
+        "diego@mail.com": {
+            "email": "doctor@mail.com",
+            "id": "kD5e6Klo5wgyQxncqAZH3BysylK2",
+            "role": "Doctor"
+        }
+    },
+    {
+        "doctor@mail.com": {
+            "email": "doctor@mail.com",
+            "id": "kD5e6Klo5wgyQxncqAZH3BysylK2",
+            "role": "Doctor"
+        }
+    },
+    {
+        "joel@hmail.com": {
+            "email": "joel@hmail.com",
+            "id": "gIcNoJZI17WNnrjUzeLP9fzAkzt2",
+            "role": "Paciente"
+        }
+    },
+    {
+        "joel@mail.com": {
+            "email": "joel@mail.com",
+            "id": "cy8BtBlf3fSH3d5fFqozX7FhXF03",
+            "role": "Paciente"
+        }
+    },
+    {
+        "manu@mail.com": {
+            "email": "manu@mail.com",
+            "id": "dAzf4feLyxabbwQUdSlBRFmbMXV2",
+            "role": "Paciente"
+        }
+    },
+    {
+        "mario@mail.com": {
+            "email": "mario@mail.com",
+            "id": "oU7plxXqVZOeOgT70RanfY6FYp52",
+            "role": "Paciente"
+        }
+    },
+    {
+        "se@mail.com": {
+            "email": "se@mail.com",
+            "id": "crCngPzHMuTFUhabp0Vnuk6e37q1",
+            "role": "Paciente"
+        }
+    },
+    {
+        "sofia@mail.com": {
+            "email": "sofia@mail.com",
+            "id": "j9bdK0Mhm4ektA0HKViQv140MGl2",
+            "role": "Paciente"
+        }
+    },
+    {
+        "supertest@tec.com": {
+            "email": "supertest@tec.com",
+            "id": "xsEdUuXAQ6PxlrIbB91f8wDkqnc2",
+            "role": "Paciente"
+        }
+    }
+];
+
+export default ROLES;
