4f49764fad9ee5d2744a9df00531743abfb5e40c
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-03-05 12:52:12 -0600

    Style cards of symptom list by adding css style.

--- src/Dashboard/SymptomList/SymptomCard.tsx
+++ src/Dashboard/SymptomList/SymptomCard.tsx
@@ -1,6 +1,8 @@
 // External libraries
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+// Own styles
+import styles from "./SymptomCard.module.css"
 
 const SINTOMA = {
     "189D70BE-CC59-45E7-8E41-E43CBC6CD8CD": {
@@ -18,12 +20,18 @@ const SINTOMA = {
 }
 
 function SymptomCard() {
+    const sintoma = SINTOMA['189D70BE-CC59-45E7-8E41-E43CBC6CD8CD'];
     return (
         <ListGroup.Item>
-            <Card>
+            <Card border="success">
                 <Card.Body>
-                    <Card.Title>{SINTOMA['189D70BE-CC59-45E7-8E41-E43CBC6CD8CD'].nombre}</Card.Title>
-                    <Card.Subtitle>{SINTOMA['189D70BE-CC59-45E7-8E41-E43CBC6CD8CD'].fecha}</Card.Subtitle>
+                    <Card.Title>{sintoma.nombre}</Card.Title>
+                    <Card.Subtitle>{sintoma.fecha}</Card.Subtitle>
+                    <div className={styles.text}>
+                        <div>{sintoma.activo ? "Activo": "Desactivado"}</div>
+                        <div>|</div>
+                        <div>{sintoma.cuantitativo ? "Cuantitativo": "Cualitativo"}</div>
+                    </div>
                 </Card.Body>
             </Card>
         </ListGroup.Item>
