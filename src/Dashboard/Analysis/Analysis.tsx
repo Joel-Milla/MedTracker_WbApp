66f60cd3b1db790032d0d46c332c2b24c1ccea75
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-03-05 12:51:30 -0600

    Create component that handles the graph.

---
+++ src/Dashboard/Analysis/Analysis.tsx
@@ -0,0 +1,14 @@
+// Own styles
+import styles from "./Analysis.module.css"
+
+function Analysis() {
+    return (
+        <>
+            <div className={`${styles.container} col col-md-6`}>
+
+            </div>
+        </>
+    )
+}
+
+export default Analysis
