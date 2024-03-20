9dc13b416ea41cd5ad26fdef6a5305b0bc7a5e8f
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-03-19 20:31:10 -0600

    Add card around selected metadata.

--- src/Dashboard/Analysis/Graph/SelectedData/SelectedData.tsx
+++ src/Dashboard/Analysis/Graph/SelectedData/SelectedData.tsx
@@ -1,3 +1,5 @@
+// External components
+import { Card } from "@nextui-org/react";
 // Own components
 import Information from './Information';
 import Notes from './Notes';
@@ -5,14 +7,16 @@ import Notes from './Notes';
 function SelectedData() {
     return (
         <>
-            <div className='grid grid-cols-12 gap-4 mt-6'>
-                <div className=' col-span-3'>
-                    <Information />
+            <Card className="p-4">
+                <div className='grid grid-cols-12 gap-4 mt-6'>
+                    <div className=' col-span-3'>
+                        <Information />
+                    </div>
+                    <div className=' col-span-9'>
+                        <Notes />
+                    </div>
                 </div>
-                <div className=' col-span-9'>
-                    <Notes />
-                </div>
-            </div>
+            </Card>
         </>
     )
 }
