dbe56d31dab7359805436b028b24334d1aa0447f
Christopher Pedraza <gabalixreborn@gmail.com>
2024-03-22 20:40:25 -0600

    Cambiar el espacio entre tarjetasd el grid

--- src/Pacientes/GridTarjetasPacientes.tsx
+++ src/Pacientes/GridTarjetasPacientes.tsx
@@ -10,7 +10,7 @@ interface GridTarjetasPacientesProps {
 function GridTarjetasPacientes({ pacientes }: GridTarjetasPacientesProps) {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={2}>
+			<Grid container spacing={4}>
 				{pacientes.map((paciente) => (
 					<Grid xs={12} sm={6} md={4} key={paciente.id}>
 						<TarjetaPaciente {...paciente} />
