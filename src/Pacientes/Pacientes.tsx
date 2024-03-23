c0b9c212f23f88d1e78db26a10864da19025fdbd
Christopher Pedraza <gabalixreborn@gmail.com>
2024-03-22 19:03:10 -0600

    Agregar datos a tarjeta y boton

--- src/Pacientes/Pacientes.tsx
+++ src/Pacientes/Pacientes.tsx
@@ -7,7 +7,13 @@ export default function Pacientes() {
 			<NavigationMenu
 				title="Pacientes"
 				selectedOption={0}>
-				<TarjetaPaciente nombre='Christopher Pedraza Pohlenz' fechaUltimoRegistro='27 febrero' />
+				<TarjetaPaciente
+					nombre='Christopher Pedraza Pohlenz'
+					fechaUltimoRegistro='27 febrero'
+					imagen='https://i.pravatar.cc/150?u=a04258114e29026302d'
+					email='christopher@mail.com'
+					celular='5544332211'
+				/>
 			</NavigationMenu>
 		</div>
 	)
--- src/Pacientes/TarjetaPaciente.tsx
+++ src/Pacientes/TarjetaPaciente.tsx
@@ -1,13 +1,16 @@
-import { Card, CardBody, CardHeader, Avatar } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Avatar } from "@nextui-org/react";
+import CircleIcon from '@mui/icons-material/Circle';
+import { ListItem, ListItemIcon, ListItemText, List, Typography } from "@mui/material";
 
 import "./TarjetaPaciente.css";
 
+
 interface TarjetaPacienteProps {
-	imagen?: string;
-	nombre?: string;
-	fechaUltimoRegistro?: string;
-	email?: string;
-	celular?: string;
+	imagen: string;
+	nombre: string;
+	fechaUltimoRegistro: string;
+	email: string;
+	celular: string;
 	onClick?: () => void;
 }
 
@@ -15,16 +18,44 @@ function TarjetaPaciente(props: TarjetaPacienteProps) {
 	return (
 		<Card>
 			<CardHeader className="card-header">
-				<Avatar isBordered radius="lg" src="https://i.pravatar.cc/150?u=a04258114e29026302d" className="avatar-paciente" />
+				<Avatar isBordered radius="lg" src={props.imagen} className="avatar-paciente" />
 				<div>
 					<p className="nombre-paciente">{props.nombre}</p>
 					<p className="fecha-registro">{props.fechaUltimoRegistro}</p>
 				</div>
 			</CardHeader>
 			<CardBody>
-				<p>Make beautiful websites regardless of your design experience.</p>
+				<List>
+					<ListItemElement icon={CircleIcon} text="Email registrado: " value={props.email} />
+					<ListItemElement icon={CircleIcon} text="Telefono celular: " value={props.celular} />
+				</List>
+				<Button color="primary">
+					<strong>Ver detalles</strong>
+				</Button>
 			</CardBody>
 		</Card >
+	);
+}
+
+function ListItemElement(props: { icon: any, text: string, value: string }) {
+	return (
+		<ListItem key={props.text} disablePadding>
+			<ListItemIcon sx={{ color: 'primary.main' }}>
+				{<props.icon fontSize="small" />}
+			</ListItemIcon>
+			<ListItemText
+				primary={
+					<>
+						<Typography variant="body1" component="span" fontWeight="bold">
+							{props.text}
+						</Typography>
+						<Typography variant="body1" component="span" color="text.secondary">
+							{props.value}
+						</Typography>
+					</>
+				}
+			/>
+		</ListItem>
 	);
 }
 
