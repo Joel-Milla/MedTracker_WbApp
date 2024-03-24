5246c3a5af0f28f634dec8817b6b67bf039bdb8b
Joel-Milla <jalejandro.milla@outlook.com>
2024-03-23 22:18:45 -0600

    Add responsivenes for mobiel and larger screens for data insihts.

--- src/Dashboard/Analysis/Insights/Insights.tsx
+++ src/Dashboard/Analysis/Insights/Insights.tsx
@@ -9,11 +9,11 @@ function Insights() {
         <>
             <Card
                 key="1"
-                className="grid grid-cols-4 p-4 w-full mb-10" >
-                <div className='col-span-2'>
+                className="flex flex-col p-4 w-full mb-10 lg:grid lg:grid-cols-4" >
+                <div className='lg:col-span-2'>
                     <DataValues />
                 </div>
-                <div className='col-span-2'>
+                <div className='lg:col-span-2'>
                     <DataGraph />
                 </div>
             </Card>
