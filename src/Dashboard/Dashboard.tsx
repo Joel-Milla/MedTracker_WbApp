539056e58a7e9bbedec848591623653501388a8e
Joel-Milla <jalejandro.milla@outlook.com>
2024-03-23 22:01:52 -0600

    Make resopnsive the dashboard.

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -9,11 +9,11 @@ function Dashboard() {
         <>
             <div className='container mx-auto mb-10'>
                 <Title />
-                <div className="grid grid-cols-12 gap-3 justify-center mt-10">
-                    <div className="col-span-3">
+                <div className="flex flex-col gap-3 justify-center mt-10 md:grid md:grid-cols-12">
+                    <div className="md:col-span-4 lg:col-span-3">
                         <Symptoms />
                     </div>
-                    <div className="col-span-9">
+                    <div className="md:col-span-8 lg:col-span-9">
                         <Analysis />
                         <UserInformation />
                     </div>
