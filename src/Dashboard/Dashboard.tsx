244fa958cac634ce9684e5faf89d0264be266100
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-03-27 06:19:06 -0600

    Add responsiveness to automcplete on main dashboard.

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -9,12 +9,13 @@ function Dashboard() {
         <>
         {/* Show the three main parts of the dashboard, the title that contains the current user, the list of symptoms with the graph to see the symptom selected and then the current information of the user.  */}
         {/* Container to limit the size, mx-auto to center the div and marginBottom of 10 to have white space at the end */}
-            <div className='container mx-auto mb-10'>
+        {/* Add padding for display to not cover fully the view */}
+            <div className='container mx-auto mb-10 p-3'>
                 <Header />
                 {/* Flex to have them in same row and have it expanded full parent size*/}
-                <div className="flex gap-3 w-full mt-5">
-                    {/* Limit the width of symptoms to 1/4 of the parent size */}
-                    <div className="w-1/4"> 
+                <div className="flex flex-col gap-3 mt-5 md:flex-row">
+                    {/* Responsive width depending on screen size */}
+                    <div className="md:w-1/3 2xl:w-1/4"> 
                         <Symptoms />
                     </div>
                     {/* Expand the dashboard the all the available width */}
--- src/Dashboard/Header/Header.tsx
+++ src/Dashboard/Header/Header.tsx
@@ -6,8 +6,8 @@ function Header() {
   return (
     // Flex to expand the div 100% of container and align items as a column with multiple rows
     <div className="flex flex-col">
-      {/* Add margins to clearly define the current user. Set 1/4 to the width of the element*/}
-      <div className="w-1/4 mt-5 mb-9">
+      {/* Add margins to clearly define the current user. Set same width as symptoms list based on screen size*/}
+      <div className="md:w-1/3 2xl:w-1/4 mt-5 mb-9">
         <CurrentPatient />
       </div>
       {/* Because of flex properties, expand the full width of container */}
