4a88aac7de18c8ac0d9202060f71221b31c35d82
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-04-01 20:59:24 -0600

    Show all the patients in a selector.

--- src/Dashboard/Header/CurrentPatient/CurrentPatient.tsx
+++ src/Dashboard/Header/CurrentPatient/CurrentPatient.tsx
@@ -1,48 +1,33 @@
 // External components
 import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
-
-// Mock data to show names as an option
-const names = [
-  {
-    value: "Joel Milla",
-    label: "Joel Milla"
-  },
-  {
-    value: "Diego Velazquez",
-    label: "Diego Velazquez"
-  }
-  ,
-  {
-    value: "Angela",
-    label: "Angela"
-  }
-  ,
-  {
-    value: "Sebastian",
-    label: "Sebastian"
-  },
-  {
-    value: "Christopher",
-    label: "Christopher"
-  }
-]
+// Redux connection
+import { RootState } from "../../../state/store";
+import { useSelector } from "react-redux";
 
 function CurrentPatient() {
-    return (
-        <>
-        {/* Use the autocomplete of nextUI to select from all the mock users */}
-        <Autocomplete
-                color="primary"
-                label="Buscar"
-                variant="bordered" // This element makes the search bar transparent
-                defaultItems={names}
-                placeholder="Escoge dato salud"
-            >
-              {/* Map the names as an item to be shown and selected */}
-                {(name) => <AutocompleteItem key={name.value}>{name.label}</AutocompleteItem>}
-            </Autocomplete>
-        </>
-    )
+  // Obtain the current users
+  const patients = useSelector((state: RootState) => state.patients);
+  const names = patients.map(patient => {
+    return {
+      value: patient.email,
+      label: patient.name
+    }
+  });
+  return (
+    <>
+      {/* Use the autocomplete of nextUI to select from all the mock users */}
+      <Autocomplete
+        color="primary"
+        label="Buscar"
+        variant="bordered" // This element makes the search bar transparent
+        defaultItems={names}
+        placeholder="Escoge al usuario"
+      >
+        {/* Map the names as an item to be shown and selected */}
+        {(name) => <AutocompleteItem key={name.value}>{name.label}</AutocompleteItem>}
+      </Autocomplete>
+    </>
+  )
 }
 
 export default CurrentPatient;
--- src/Models/Patients.ts
+++ src/Models/Patients.ts
@@ -6,103 +6,23 @@ interface Patient {
 
 export const mockData: Patient[] = [
     {
-        idDocFirebase: 'example1@example.com',
-        email: 'example1@example.com',
-        name: 'John Doe'
+        idDocFirebase: 'joel@mail.com',
+        email: 'joel@mail.com',
+        name: 'Joel'
     },
     {
-        idDocFirebase: 'example2@example.com',
-        email: 'example2@example.com',
-        name: 'Jane Smith'
+        idDocFirebase: 'diego@mail.com',
+        email: 'diego@mail.com',
+        name: 'Diego'
     },
     {
-        idDocFirebase: 'example3@example.com',
-        email: 'example3@example.com',
-        name: 'Alice Johnson'
+        idDocFirebase: 'manu@mail.com',
+        email: 'manu@mail.com',
+        name: 'Manu'
     },
     {
-        idDocFirebase: 'example4@example.com',
-        email: 'example4@example.com',
-        name: 'Bob Brown'
+        idDocFirebase: 'mario@mail.com',
+        email: 'mario@mail.com',
+        name: 'Mario'
     },
-    {
-        idDocFirebase: 'example5@example.com',
-        email: 'example5@example.com',
-        name: 'Sarah Lee'
-    },
-    {
-        idDocFirebase: 'example6@example.com',
-        email: 'example6@example.com',
-        name: 'Michael Clark'
-    },
-    {
-        idDocFirebase: 'example7@example.com',
-        email: 'example7@example.com',
-        name: 'Emily Davis'
-    },
-    {
-        idDocFirebase: 'example8@example.com',
-        email: 'example8@example.com',
-        name: 'David Wilson'
-    },
-    {
-        idDocFirebase: 'example9@example.com',
-        email: 'example9@example.com',
-        name: 'Olivia Martinez'
-    },
-    {
-        idDocFirebase: 'example10@example.com',
-        email: 'example10@example.com',
-        name: 'Daniel Anderson'
-    },
-    {
-        idDocFirebase: 'example11@example.com',
-        email: 'example11@example.com',
-        name: 'Sophia Rodriguez'
-    },
-    {
-        idDocFirebase: 'example12@example.com',
-        email: 'example12@example.com',
-        name: 'Matthew Taylor'
-    },
-    {
-        idDocFirebase: 'example13@example.com',
-        email: 'example13@example.com',
-        name: 'Ava Moore'
-    },
-    {
-        idDocFirebase: 'example14@example.com',
-        email: 'example14@example.com',
-        name: 'Ethan Thomas'
-    },
-    {
-        idDocFirebase: 'example15@example.com',
-        email: 'example15@example.com',
-        name: 'Isabella Jackson'
-    },
-    {
-        idDocFirebase: 'example16@example.com',
-        email: 'example16@example.com',
-        name: 'Noah White'
-    },
-    {
-        idDocFirebase: 'example17@example.com',
-        email: 'example17@example.com',
-        name: 'Mia Harris'
-    },
-    {
-        idDocFirebase: 'example18@example.com',
-        email: 'example18@example.com',
-        name: 'Liam Martin'
-    },
-    {
-        idDocFirebase: 'example19@example.com',
-        email: 'example19@example.com',
-        name: 'Charlotte Thompson'
-    },
-    {
-        idDocFirebase: 'example20@example.com',
-        email: 'example20@example.com',
-        name: 'James Garcia'
-    }
 ]
