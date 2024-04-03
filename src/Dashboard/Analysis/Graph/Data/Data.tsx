c894eefb93e038bf204aba2dc86994c2de197e81
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-04-03 11:36:14 -0600

    Add the tabbar to the main component view.

--- src/Dashboard/Analysis/Graph/Data/Data.tsx
+++ src/Dashboard/Analysis/Graph/Data/Data.tsx
@@ -1,4 +1,5 @@
 // Own components
+import TabBar from '../Chart/TabBar';
 import Chart from '../Chart/Chart';
 import SelectedData from '../SelectedData/SelectedData';
 
@@ -7,6 +8,7 @@ function Data() {
         <>
         {/* Show the chart and the selected data together. Add also a margin top of the selected data between the view and the chart */}
             <div>
+                <TabBar />
                 <Chart />
             </div>
             <div className='mt-6'>
