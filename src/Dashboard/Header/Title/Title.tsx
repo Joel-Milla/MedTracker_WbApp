6ff1ee4221a49fedae7465001518c939f489aa69
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-04-03 19:57:43 -0600

    Show in total the amount of symptoms that exist.

--- src/Dashboard/Header/Title/Title.tsx
+++ src/Dashboard/Header/Title/Title.tsx
@@ -1,16 +1,23 @@
+// Redux connection
+import { RootState } from "../../../state/store";
+import { useSelector } from "react-redux";
 // External Components
 import { Badge } from '@tremor/react';
 // Own Components
 import DelimiterPersianGreen from './DelimiterPersianGreen';
 
 function Title() {
+    // Get the symptoms of the current user
+    const symptoms = useSelector((state: RootState) => state.user.symptoms);
+    const symptomsLength = symptoms.length;
+
     return (
         <>
-        {/* Show the title with a bage that says the current amount of symptoms shared */}
-        {/* Use flex with gap of 3 to have the items in a single row with some space between them. Add margin bottom to have space between title and delimiter */}
+            {/* Show the title with a bage that says the current amount of symptoms shared */}
+            {/* Use flex with gap of 3 to have the items in a single row with some space between them. Add margin bottom to have space between title and delimiter */}
             <div className="flex gap-3 mb-2">
                 <h3 className="text-3xl">Datos Compartidos</h3>
-                <Badge size="xs">6</Badge> 
+                <Badge size="xs">{symptomsLength}</Badge>
             </div>
             {/* Use the delimiter with custom color persian green that expands full width */}
             <DelimiterPersianGreen />
