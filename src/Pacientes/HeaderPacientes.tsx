a39f151da9ac115982dd3aba818928802579a598
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-04 08:53:43 -0600

    Agregar linea divisora y cambiar diseño de buscador

--- src/Pacientes/HeaderPacientes.tsx
+++ src/Pacientes/HeaderPacientes.tsx
@@ -1,18 +1,21 @@
-// Own components
-import CurrentPatient from "./CurrentPatient/CurrentPatient";
-import Title from "./Title/Title";
+import Title from "../Dashboard/Header/Title/Title";
+import SearchBar from "./SearchBar";
 
-function Header() {
+interface HeaderProps {
+    cantidadPacientes: number;
+}
+
+function Header({ cantidadPacientes }: HeaderProps) {
     return (
         // Flex to expand the div 100% of container and align items as a column with multiple rows
         <div className="flex flex-col">
             {/* Add margins to clearly define the current user. Set same width as symptoms list based on screen size*/}
-            <div className="md:w-1/3 2xl:w-1/4 mt-5 mb-9">
-                <CurrentPatient />
+            <div className="w-full mt-5 mb-9">
+                <SearchBar />
             </div>
             {/* Because of flex properties, expand the full width of container */}
             <div>
-                <Title />
+                <Title titulo="Pacientes" badge={cantidadPacientes} />
             </div>
         </div>
     );
--- src/Pacientes/TarjetaPaciente.tsx
+++ src/Pacientes/TarjetaPaciente.tsx
@@ -1,62 +1,94 @@
 import { Button, Card, CardBody, CardHeader, Avatar } from "@nextui-org/react";
-import CircleIcon from '@mui/icons-material/Circle';
-import { ListItem, ListItemIcon, ListItemText, List, Typography } from "@mui/material";
+import CircleIcon from "@mui/icons-material/Circle";
+import {
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    List,
+    Typography,
+} from "@mui/material";
 
 import "./TarjetaPaciente.css";
-
+import BlueDot from "../Dashboard/Symptoms/SymptomList/BlueDot";
 
 interface TarjetaPacienteProps {
-	id: number;
-	imagen: string;
-	nombre: string;
-	fechaUltimoRegistro: string;
-	email: string;
-	celular: string;
+    id: number;
+    imagen: string;
+    nombre: string;
+    fechaUltimoRegistro: string;
+    email: string;
+    celular: string;
 }
 
 function TarjetaPaciente(props: TarjetaPacienteProps) {
-	return (
-		<Card className="tarjeta-paciente">
-			<CardHeader className="card-header">
-				<Avatar isBordered radius="lg" src={props.imagen} className="avatar-paciente" />
-				<div>
-					<p className="nombre-paciente">{props.nombre}</p>
-					<p className="fecha-registro">{props.fechaUltimoRegistro}</p>
-				</div>
-			</CardHeader>
-			<CardBody>
-				<List>
-					<ListItemElement icon={CircleIcon} text="Email: " value={props.email} />
-					<ListItemElement icon={CircleIcon} text="Teléfono: " value={props.celular} />
-				</List>
-				<Button color="primary">
-					<strong>Ver detalles</strong>
-				</Button>
-			</CardBody>
-		</Card >
-	);
+    return (
+        <Card className="tarjeta-paciente">
+            <CardHeader className="card-header">
+                <Avatar
+                    isBordered
+                    radius="lg"
+                    src={props.imagen}
+                    className="avatar-paciente"
+                />
+                <div>
+                    <p className="nombre-paciente">{props.nombre}</p>
+                    <p className="fecha-registro">
+                        {props.fechaUltimoRegistro}
+                    </p>
+                </div>
+            </CardHeader>
+            <CardBody>
+                <List>
+                    <ListItemElement
+                        icon={CircleIcon}
+                        text="Email: "
+                        value={props.email}
+                    />
+                    <ListItemElement
+                        icon={CircleIcon}
+                        text="Teléfono: "
+                        value={props.celular}
+                    />
+                </List>
+                <Button color="primary">
+                    <strong>Ver detalles</strong>
+                </Button>
+            </CardBody>
+        </Card>
+    );
 }
 
-function ListItemElement(props: { icon: any, text: string, value: string }) {
-	return (
-		<ListItem key={props.text} disablePadding>
-			<ListItemIcon sx={{ color: 'primary.main' }}>
-				{<props.icon fontSize="small" />}
-			</ListItemIcon>
-			<ListItemText
-				primary={
-					<>
-						<Typography variant="body1" component="span" fontWeight="bold">
-							{props.text}
-						</Typography>
-						<Typography variant="body1" component="span" color="text.secondary">
-							{props.value}
-						</Typography>
-					</>
-				}
-			/>
-		</ListItem>
-	);
+function ListItemElement(props: { icon: any; text: string; value: string }) {
+    return (
+        <ListItem key={props.text} disablePadding>
+            <BlueDot />
+            <p className="font-semibold">&nbsp;{props.text}&nbsp;</p>
+            <p>{props.value}</p>
+            {/* <ListItemIcon sx={{ color: "primary.main" }}>
+                {<props.icon fontSize="xs" />}
+            </ListItemIcon>
+            <ListItemText
+                primary={
+                    <>
+                        <Typography
+                            variant="body1"
+                            component="span"
+                            fontWeight="bold"
+                        >
+                            {props.text}
+                        </Typography>
+                        <Typography
+                            variant="body1"
+                            component="span"
+                            color="text.secondary"
+                        >
+                            {props.value}
+                        </Typography>
+                    </>
+                }
+            /> */}
+        </ListItem>
+    );
 }
 
-export default TarjetaPaciente
+export default TarjetaPaciente;
