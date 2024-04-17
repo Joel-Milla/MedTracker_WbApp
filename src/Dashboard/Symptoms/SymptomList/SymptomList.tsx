7820ba1da0d3d90a3cd83d6b4452221f05a3c356
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-04-17 14:10:30 -0600

    Add loading state.

--- src/Dashboard/Symptoms/SymptomList/SymptomList.tsx
+++ src/Dashboard/Symptoms/SymptomList/SymptomList.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { RootState } from "../../../state/store";
 import { useDispatch, useSelector } from "react-redux";
 // External components
-import { Listbox, ListboxItem, Skeleton } from "@nextui-org/react";
+import { Listbox, ListboxItem } from "@nextui-org/react";
 // Components
 import SymptomCard from "./SymptomCard";
 import { setSelectedSymptoms } from "../../../state/Slices/uiSlice";
@@ -18,8 +18,6 @@ function SymptomList() {
     (state: RootState) => state.user.patientData.symptoms
   );
   const searchText = useSelector((state: RootState) => state.ui.searchText);
-  // Loading state
-  const isLoading = useSelector((state: RootState) => state.user.isLoading);
 
   // Save the selected key and its initial state
   const initialSelection = symptoms.length > 0 ? new Set([]) : new Set([]);
@@ -63,13 +61,8 @@ function SymptomList() {
                 color="primary"
                 textValue={symptom.nombre}
               >
-                <Skeleton
-                  isLoaded={!isLoading}
-                  className="rounded-lg bg-persian-green-500"
-                >
-                  {/* Show a symptom card as the value */}
-                  <SymptomCard symptom={symptom} />
-                </Skeleton>
+                {/* Show a symptom card as the value */}
+                <SymptomCard symptom={symptom} />
               </ListboxItem>
             );
           })}
--- src/Pacientes/SearchBar.tsx
+++ src/Pacientes/SearchBar.tsx
@@ -2,14 +2,19 @@
 import { RootState } from "../state/store";
 import { useDispatch, useSelector } from "react-redux";
 import { setPatientSearchText } from "../state/Slices/uiSlice";
-// Own components
-import { Autocomplete, AutocompleteItem, Avatar } from "@nextui-org/react";
-// Mock data
-import { data } from "./Paciente";
+// External components
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Avatar,
+  Skeleton,
+} from "@nextui-org/react";
 
 function SearchBar() {
   // Obtain the current patients
   const patients = useSelector((state: RootState) => state.patients.pacientes);
+  // Loading state
+  const isLoading = useSelector((state: RootState) => state.patients.isLoading);
   // Use this hook to dispatch actions to redux
   const dispatch = useDispatch();
 
@@ -19,39 +24,44 @@ function SearchBar() {
   };
 
   return (
-    <Autocomplete
-      allowsCustomValue
-      color="primary"
-      label="Buscar"
-      variant="bordered" // This element makes the search bar transparent
-      placeholder="Escoge al usuario"
-      defaultItems={patients}
-      onKeyDown={(e: any) => e.continuePropagation()} // stops an error from loggin on the console about 'stopPropagation'.
-      onInputChange={handleInputChange}
-      aria-label="Selecciona a un paciente"
+    <Skeleton
+      isLoaded={!isLoading}
+      className="w-full rounded-lg bg-persian-green-500"
     >
-      {(patient) => (
-        <AutocompleteItem key={patient.email} textValue={patient.name}>
-          <div className="flex justify-between items-center">
-            <div className="flex gap-2 items-center">
-              <Avatar
-                alt={patient.name}
-                className="flex-shrink-0"
-                size="sm"
-                src=""
-                // Show the initials of the name
-                name={patient.name}
-                showFallback
-                color="primary"
-              />
-              <div className="flex flex-col">
-                <span className="text-small">{patient.name}</span>
+      <Autocomplete
+        allowsCustomValue
+        color="primary"
+        label="Buscar"
+        variant="bordered" // This element makes the search bar transparent
+        placeholder="Escoge al usuario"
+        defaultItems={patients}
+        onKeyDown={(e: any) => e.continuePropagation()} // stops an error from loggin on the console about 'stopPropagation'.
+        onInputChange={handleInputChange}
+        aria-label="Selecciona a un paciente"
+      >
+        {(patient) => (
+          <AutocompleteItem key={patient.email} textValue={patient.name}>
+            <div className="flex justify-between items-center">
+              <div className="flex gap-2 items-center">
+                <Avatar
+                  alt={patient.name}
+                  className="flex-shrink-0"
+                  size="sm"
+                  src=""
+                  // Show the initials of the name
+                  name={patient.name}
+                  showFallback
+                  color="primary"
+                />
+                <div className="flex flex-col">
+                  <span className="text-small">{patient.name}</span>
+                </div>
               </div>
             </div>
-          </div>
-        </AutocompleteItem>
-      )}
-    </Autocomplete>
+          </AutocompleteItem>
+        )}
+      </Autocomplete>
+    </Skeleton>
   );
 }
 
