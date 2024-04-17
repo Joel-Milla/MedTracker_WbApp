465264a6f4ab146f14c9287421950ceb1a51802d
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-04-17 11:54:56 -0600

    Create function that brings the info of the patient.

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,32 +1,41 @@
+// Redux connection
+import { useDispatch } from "react-redux";
+import { fetchPatientInformation } from "../state/Slices/userSlice";
 // Components
 import Header from "./Header/Header";
 import Symptoms from "./Symptoms/Symptoms";
 import Analysis from "./Analysis/Analysis";
 import UserInformation from "./UserInformation/UserInformation";
+import { useEffect } from "react";
 
 function Dashboard() {
-    return (
-        <>
-        {/* Show the three main parts of the dashboard, the title that contains the current user, the list of symptoms with the graph to see the symptom selected and then the current information of the user.  */}
-        {/* Container to limit the size, mx-auto to center the div and marginBottom of 10 to have white space at the end */}
-        {/* Add padding for display to not cover fully the view */}
-            <div className='container mx-auto mb-10 p-3'>
-                <Header />
-                {/* Flex to have them in same row and have it expanded full parent size*/}
-                <div className="flex flex-col gap-3 mt-5 md:flex-row">
-                    {/* Responsive width depending on screen size */}
-                    <div className="md:w-1/3 2xl:w-1/4"> 
-                        <Symptoms />
-                    </div>
-                    {/* Expand the dashboard the all the available width */}
-                    <div className="w-full">
-                        <Analysis />
-                        <UserInformation />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPatientInformation());
+  });
+
+  return (
+    <>
+      {/* Show the three main parts of the dashboard, the title that contains the current user, the list of symptoms with the graph to see the symptom selected and then the current information of the user.  */}
+      {/* Container to limit the size, mx-auto to center the div and marginBottom of 10 to have white space at the end */}
+      {/* Add padding for display to not cover fully the view */}
+      <div className="container mx-auto mb-10 p-3">
+        <Header />
+        {/* Flex to have them in same row and have it expanded full parent size*/}
+        <div className="flex flex-col gap-3 mt-5 md:flex-row">
+          {/* Responsive width depending on screen size */}
+          <div className="md:w-1/3 2xl:w-1/4">
+            <Symptoms />
+          </div>
+          {/* Expand the dashboard the all the available width */}
+          <div className="w-full">
+            <Analysis />
+            <UserInformation />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
--- src/state/Slices/userSlice.ts
+++ src/state/Slices/userSlice.ts
@@ -1,9 +1,116 @@
+// This code is for calling the patients of the doctor
+import { db } from "../FirebaseConfig/config";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 // External libaries
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, PayloadAction, createSlice } from "@reduxjs/toolkit";
 // Import types/models
 import { User } from "../../Models/User";
 // Import data
 import USERS from "../../assets/MockData/UsersData";
+import { Register, Symptom } from "../../Models/Symptom_Register";
+
+export const fetchPatientInformation = createAsyncThunk(
+  "user/fetchPatientInformation",
+  async () => {
+    const email = "joel@mail.com";
+    let patient: User;
+    const symptomsCollectionRef = collection(db, "Users", email, "symptoms");
+    const registersCollectionRef = collection(db, "Users", email, "registers");
+    const patientsCollectionRef = doc(db, "Users", email);
+    try {
+      // Get the symptoms of the user
+      const symptomsQuerySnapshot = await getDocs(symptomsCollectionRef);
+      const symptoms: Symptom[] = symptomsQuerySnapshot.docs.map((doc) => ({
+        idDocFirebase: doc.id,
+        fecha: doc.data().creationDate || { seconds: 0, nanoseconds: 0 },
+        color: doc.data().color || "",
+        cuantitativo: doc.data().isQuantitative || "",
+        unidades: doc.data().units || "",
+        icon: doc.data().icon || "",
+        description: doc.data().description || "",
+        id: doc.data().id || "",
+        nombre: doc.data().name || "",
+        notificacion: doc.data().notification || "",
+        activo: doc.data().isFavorite || "",
+      }));
+
+      // Get the register of the user
+      const registersQuerySnapshot = await getDocs(registersCollectionRef);
+      const registers: Register[] = registersQuerySnapshot.docs.map((doc) => ({
+        idDocFirebase: doc.id,
+        fecha: doc.data().date || { seconds: 0, nanoseconds: 0 },
+        idSymptom: doc.data().idSymptom || "",
+        notas: doc.data().notes || "",
+        cantidad: doc.data().amount || 0,
+        id: doc.data().id || "",
+      }));
+
+      // Get patient info
+      const patientQuerySnapshot = await getDoc(patientsCollectionRef);
+      if (patientQuerySnapshot.exists()) {
+        patient = {
+          idDocFirebase: email,
+          arregloDoctor: patientQuerySnapshot.data().doctors || [],
+          email: patientQuerySnapshot.data().email || "",
+          rol: patientQuerySnapshot.data().rol || "",
+          sexo: patientQuerySnapshot.data().sex || "",
+          antecedentes: patientQuerySnapshot.data().clinicalHistory || "",
+          nombreCompleto: patientQuerySnapshot.data().name || "",
+          fechaNacimiento: patientQuerySnapshot.data().birthdate || {
+            seconds: 0,
+            nanoseconds: 0,
+          },
+          telefono: patientQuerySnapshot.data().phone || "",
+          estatura: patientQuerySnapshot.data().height || "",
+          id: patientQuerySnapshot.data().id || "",
+          symptoms: symptoms,
+          registers: registers,
+        };
+      } else {
+        patient = {
+          idDocFirebase: "",
+          arregloDoctor: [],
+          email: "",
+          rol: "",
+          sexo: "",
+          antecedentes: "",
+          nombreCompleto: "",
+          fechaNacimiento: {
+            seconds: 0,
+            nanoseconds: 0,
+          },
+          telefono: "",
+          estatura: "",
+          id: "",
+          symptoms: symptoms,
+          registers: registers,
+        };
+      }
+
+      return patient;
+    } catch (error) {
+      console.log("[userSlice] Error fetching patient info: ", error);
+    }
+    return {
+      idDocFirebase: "",
+      arregloDoctor: [],
+      email: "",
+      rol: "",
+      sexo: "",
+      antecedentes: "",
+      nombreCompleto: "",
+      fechaNacimiento: {
+        seconds: 0,
+        nanoseconds: 0,
+      },
+      telefono: "",
+      estatura: "",
+      id: "",
+      symptoms: [],
+      registers: [],
+    };
+  }
+);
 
 const selectedPatient = "";
 const patientDataInitialState: User = {
@@ -30,6 +137,8 @@ export const userSlice = createSlice({
   initialState: {
     selectedPatient: selectedPatient,
     patientData: patientDataInitialState,
+    isLoading: false,
+    hasError: false,
   },
   reducers: {
     setSelectedPatient: (state, action: PayloadAction<string>) => {
@@ -42,6 +151,23 @@ export const userSlice = createSlice({
         }
       });
     },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPatientInformation.pending, (state) => {
+        state.isLoading = true;
+        state.hasError = false;
+      })
+      .addCase(fetchPatientInformation.rejected, (state) => {
+        state.isLoading = false;
+        state.hasError = true;
+      })
+      .addCase(fetchPatientInformation.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.hasError = false;
+        state.patientData = action.payload;
+      });
   },
 });
 
