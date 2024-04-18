ed358ff2e8b4c4957331b8b8fada7c9a0fcaecc6
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-18 09:23:25 -0600

    Update ProfileButton component to include a white background color

--- src/Profile/ProfileButon/ProfileButton.tsx
+++ src/Profile/ProfileButon/ProfileButton.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@nextui-org/react";
 
 interface EditButtonProps {
-  onClick: () => void;
-  value: string;
+    onClick: () => void;
+    value: string;
 }
 
 function EditButton({ onClick, value }: EditButtonProps) {
-  return <Button onClick={onClick}>{value}</Button>;
+    return (
+        <Button onClick={onClick} style={{ backgroundColor: "white" }}>
+            {value}
+        </Button>
+    );
 }
 
 export default EditButton;
