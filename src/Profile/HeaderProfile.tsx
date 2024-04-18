3a5131aa7ad2b1acdce378874743ccef283509f8
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-18 09:11:21 -0600

    Cambiar diseño de textfield de login

--- src/Profile/HeaderProfile.tsx
+++ src/Profile/HeaderProfile.tsx
@@ -2,15 +2,15 @@
 import Title from "../Dashboard/Header/Title/Title";
 
 function Header() {
-  return (
-    // Flex to expand the div 100% of container and align items as a column with multiple rows
-    <div className="flex flex-col">
-      {/* Add margins to clearly define the current user. Set same width as symptoms list based on screen size*/}
-      <div>
-        <Title titulo="Perfil" />
-      </div>
-    </div>
-  );
+    return (
+        // Flex to expand the div 100% of container and align items as a column with multiple rows
+        <div className="flex flex-col mb-4 mt-4">
+            {/* Add margins to clearly define the current user. Set same width as symptoms list based on screen size*/}
+            <div>
+                <Title titulo="Perfil" />
+            </div>
+        </div>
+    );
 }
 
 export default Header;
--- src/Profile/ProfileField.tsx
+++ src/Profile/ProfileField.tsx
@@ -1,37 +1,37 @@
 import { Input } from "@nextui-org/react";
 
 interface ProfileFieldProps {
-  label: string;
-  value: string;
-  setValue: (value: string) => void;
-  editable: boolean;
-  placeholder: string;
+    label: string;
+    value: string;
+    setValue: (value: string) => void;
+    editable: boolean;
+    placeholder: string;
 }
 
 function ProfileField({
-  label,
-  value,
-  setValue,
-  editable,
-  placeholder,
+    label,
+    value,
+    setValue,
+    editable,
+    placeholder,
 }: ProfileFieldProps) {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
 
-  return (
-    <div>
-      <span className="text-lg font-semibold">{label}</span>
-      <Input
-        type="text"
-        value={value}
-        className="w-full"
-        placeholder={placeholder}
-        onChange={handleChange}
-        isReadOnly={!editable}
-      />
-    </div>
-  );
+    return (
+        <div className="mb-4">
+            <span className="text-lg font-semibold">{label}</span>
+            <Input
+                type="text"
+                value={value}
+                className="w-full border-primary border-2 rounded-xl background-transparent"
+                placeholder={placeholder}
+                onChange={handleChange}
+                isReadOnly={!editable}
+            />
+        </div>
+    );
 }
 
 export default ProfileField;
