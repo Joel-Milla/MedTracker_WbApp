a2c79f66244117c348bad741f4c0f3a14d5d8e97
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-04-26 07:09:52 -0600

    Show loading state and navigate away if user is on bad route.

--- src/GlobalNavigation/Root.tsx
+++ src/GlobalNavigation/Root.tsx
@@ -1,23 +1,32 @@
 // Redux connectinos
-import { useDispatch } from "react-redux";
+import { RootState } from "../state/store";
+import { useSelector, useDispatch } from "react-redux";
 // External functions
 import { onAuthStateChanged } from "firebase/auth";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // Own functions
 import {
   setCurrentUserEmail,
   setCurrentUserName,
 } from "../state/Slices/currentUserSlice";
 import { auth } from "../state/FirebaseConfig/config";
+// External components
+import { Spinner } from "@nextui-org/react";
 // Own components
 import NavigationMenu from "./NavigationMenu";
 
 function Root() {
+  // Obtain the current logged user
+  const currentUserEmail = useSelector(
+    (state: RootState) => state.currentUser.currentUserEmail
+  );
   const { pathname } = useLocation();
-  const isAuthenticated =
+  const isOnAuthViews =
     pathname == "/signup" || pathname == "/login" || pathname == "/"; // check if current user is in sign up, in the root
 
+  // Loading state to now show the bad route
+  const [loading, setLoading] = useState(true);
   // The next function is for a listener during the whole application to check if the user changed
   // Variable to navigate between views
   const navigate = useNavigate();
@@ -38,15 +47,32 @@ function Root() {
         dispatch(setCurrentUserName(""));
         navigate("/");
       }
+      setLoading(false);
     });
 
     return unsubscribe;
   }, [dispatch, navigate]);
+
+  // Make a check if the user is trying to access other routes without permission
+  if (!currentUserEmail && !isOnAuthViews) {
+    navigate("/");
+  }
+
+  // If is still loading, just show another view of loading state
+  if (loading) {
+    return (
+      <>
+        <div className="flex justify-center items-center w-full h-screen">
+          <Spinner label="Cargando..." color="primary" />
+        </div>
+      </>
+    );
+  }
   return (
     <>
       {/* Don't show the sidebar when you are on authentication */}
-      {isAuthenticated && <Outlet />}
-      {!isAuthenticated && (
+      {isOnAuthViews && <Outlet />}
+      {!isOnAuthViews && (
         <div className="flex">
           <NavigationMenu />
           <Outlet />
